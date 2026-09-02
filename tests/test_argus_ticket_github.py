e19c9a2e4d37d830d6c88641a1ab407b2807b375
Johanna England <johanna.england@sikt.no>
2026-09-02 14:53:21 +0200

    Add function to get ticket id from ticket url

--- tests/test_argus_ticket_github.py
+++ tests/test_argus_ticket_github.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 from argus.incident.ticket.base import TicketPluginException
 from django.test import SimpleTestCase, override_settings
 
@@ -30,3 +32,25 @@ class GithubTicketPluginTests(SimpleTestCase):
 
         with self.assertRaises(TicketPluginException):
             github_plugin.import_settings()
+
+
+class GetTicketIdentifierTests(SimpleTestCase):
+    def test_get_ticket_identifier_returns_ticket_id_for_valid_url(
+        self,
+    ):
+        ticket_url = "https://github.com/Uninett/argus_ticket_github/issues/1"
+        github_plugin = GithubPlugin()
+        incident = Mock(ticket_url=ticket_url)
+
+        ticket_identifier = github_plugin.get_ticket_identifier(incident=incident)
+        self.assertEqual(ticket_identifier, "1")
+
+    def test_get_ticket_identifier_returns_ticket_url_for_url_not_following_format(
+        self,
+    ):
+        ticket_url = "https://example.com/1"
+        github_plugin = GithubPlugin()
+        incident = Mock(ticket_url=ticket_url)
+
+        ticket_identifier = github_plugin.get_ticket_identifier(incident=incident)
+        self.assertEqual(ticket_identifier, ticket_url)
